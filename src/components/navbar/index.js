6c9453a6222de810894308f307cd3ad5fff8f9dd
Connor Beales <connorj.beales@gmail.com>
2023-04-08 18:33:09 +0100

    First commit

---
+++ src/components/navbar/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './navbar.module.scss';
+
+export default function Navbar() {
+    return (
+        <div className={styles.navbar}><p>Navbar text</p></div>
+    )
+}
