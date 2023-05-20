b5a490b012829bb5ee30a23f660edb4541778293
Connor Beales <connorj.beales@gmail.com>
2023-05-20 19:30:25 +0100

    framework started

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from './navbar.module.scss';
 import { Container } from '../Container';
 
@@ -6,7 +7,10 @@ export default function Navbar() {
     return (
         <div className={styles.navbar}>
             <Container variant='large'>
-                <p>In the container</p>
+                <div className={styles.navbar__wrapper}>
+                <Image src="/google_logo.svg" alt="me" width="127" height="43" />
+                    <p>In the container</p>
+                </div>
             </Container>
         </div>
     )
