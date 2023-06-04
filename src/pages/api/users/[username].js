ab37551fd34476df96c0954034f3eebbec45bbd8
Connor Beales <connorj.beales@gmail.com>
2023-06-04 18:02:07 +0100

    basic api endpoints added

--- src/pages/api/users/[username].js
+++ src/pages/api/users/[username].js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Connor B' })
+  const { username } = req.query;
+  res.status(200).json([{ name: username }])
 }
