fdc368ee77adf853b6271c2ab450e05de70532d6
Connor Beales <connorj.beales@gmail.com>
2023-06-04 17:34:35 +0100

    basic api users styling added

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,4 +1,6 @@
 import Layout from "@/components/Layout";
+import { Container } from "@/components";
+import styles from './users.module.scss';
 
 export const getStaticProps = async () => {
 
@@ -14,16 +16,26 @@ export const getStaticProps = async () => {
 const Users = ({ users }) => {
     return (
         <Layout>
-            <div>
-                <h1>All users</h1>
-                {users.map(user => (
-                    <div key={user.id}>
-                        <a>
-                            <h3>{user.name}</h3>
-                        </a>
+            <Container variant="large">
+                <section>
+                    <h1>All users</h1>
+                    <div className={styles.userGrid}>
+
+                        {users.map(({ id, name, username, email, phone }) => (
+                            <div key={id} className={styles.user}>
+                                <a>
+                                    <h3 className="margin--8">{name}</h3>
+                                    <ul className="non-styled">
+                                        <li>Username: {username}</li>
+                                        <li>Email: {email}</li>
+                                        <li>Phone: +{phone}</li>
+                                    </ul>
+                                </a>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
+                </section>
+            </Container>
         </Layout>
     )
 }
