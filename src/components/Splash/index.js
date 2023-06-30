ccd3a27d3dac6ad6b5963e09853e9104a9180c83
Connor Beales <connorj.beales@gmail.com>
2023-06-30 15:32:11 +0100

    splash section mobile formatted

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -10,9 +10,9 @@ export const Splash = ({ backgroundImg = '' }) => {
             {backgroundImg &&
                 <Image src={backgroundImg} fill={true} />
             }
-            <Container variant='large' className={styles.wrapper}>
+            <Container variant='large'>
                 <h1 className='margin--8 white'>Hi, I'm Connor<span className="teal">.</span></h1>
-                <p className='margin--64 body--xl'>I’m a front end engineer who specializes in modern <span className="teal">clean design</span> and elegant <span className="teal">performance-driven</span> code.</p>
+                <p className='margin--48 body--xl'>I’m a front end engineer who specializes in modern <span className="teal">clean design</span> and elegant <span className="teal">performance-driven</span> code.</p>
                 <Button label={`Let's get started!`} variant='outlineWhite' href='/' />
             </Container>
         </div>
