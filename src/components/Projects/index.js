eba77b4b146c615c192ed0e4f47e6523a03bc1de
Connor Beales <connorj.beales@gmail.com>
2023-09-10 14:29:04 +0100

    projects complete minus sidebar component

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,28 +5,28 @@ import { ProjectList } from './ProjectList';
 import { Container } from '../Container';
 import Image from 'next/image';
 import { ProjectOverview } from './ProjectOverview';
+// import {useIsMobile} from "/";
+import { useIsMobile } from "../../hooks";
 
 export const Projects = () => {
     const [overviewActive, setOverviewActive] = useState(false);
     const [currentProject, setCurrentProject] = useState('biscuit');
+    const isMobile = useIsMobile;
 
     const handleClick = (e) => {
-
-        // setOverviewActive(!overviewActive);
         setCurrentProject(ProjectList[e.target.id])
-        // console.log(ProjectList[e.target.id-1].title)
         console.log(currentProject)
     }
 
     return (
         <section id="projects" className={styles.myProjects}>
-            
+
             <Container variant='large'>
                 <h2 className='margin--48 white'>Projects<span className='teal'>.</span></h2>
                 <ResponsiveMasonry
                     columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
                 >
-                    <Masonry gutter={"32px"}>
+                    <Masonry gutter={!isMobile ? "32px" : "16px"}>
                         {ProjectList.map(({ image, title, description, halfSize, tech, id }) => (
                             <a
                                 // href={'#'}
@@ -40,8 +40,8 @@ export const Projects = () => {
                                     className={`${styles.project__thumbnail} ${halfSize ? styles.halfSize : ''}`}
                                 />
                                 <div className={styles.contentBox}>
-                                    <h4 className={'white margin--8'}>{title}</h4>
-                                    <p className='body--sm white margin--16'>{description}</p>
+                                    <h4 className={`white ${!isMobile ? 'margin--8' : 'no--margin'}`}>{title}</h4>
+                                    <p className={'body--sm white margin--16'}>{description}</p>
                                     <div className={styles.techGroup}>
                                         {tech?.map((item, i) => (
                                             <div key={i} className={`${styles.pill} no--margin body--xs weight--regular col--dark`}><span>{item}</span></div>
