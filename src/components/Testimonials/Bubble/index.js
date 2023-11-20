aa31f2b36786fa5a779bfb420189f360116b4ba1
Connor Beales <connorj.beales@gmail.com>
2023-11-20 16:56:12 +0000

    Testimonials section started:

--- src/components/Testimonials/Bubble/index.js
+++ src/components/Testimonials/Bubble/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './bubble.module.scss';
 
-export const Bubble = ({ content, isBlue = false }) => {
+export const Bubble = ({ content, isBlue = false, client, clientImg }) => {
     return (
-        <div className={`${styles.bubble} ${isBlue ? styles.blue : ''}`}>
-            {content}
+        <div className={styles.wrapper}>
+            <div className={styles.image} />
+            <div className={`${styles.bubble} ${isBlue ? styles.blue : ''}`}>
+                {content}
+            </div>
         </div>
     )
 }
