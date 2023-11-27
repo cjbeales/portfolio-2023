479a69a51eb59ee0abe6457a629fdd1fe64f639f
Connor Beales <connorj.beales@gmail.com>
2023-11-27 20:38:42 +0000

    Splash altered

--- src/components/TechStack/index.js
+++ src/components/TechStack/index.js
@@ -4,12 +4,15 @@ import { techStackData } from './tileData';
 
 export const TechStack = () => {
     return (
+        <>
         <div className={styles.skillset__wrapper}>
+        <p className='no--margin bold white fs--16'>Tech stack</p>
             <div className={styles.skillset__column}>
                 {techStackData.map(({ title, icon, iconWidth, imgSrc }) => (
                     <Tile key={title} title={title} icon={icon} iconWidth={iconWidth} imgSrc={imgSrc} />
                 ))}
             </div>
         </div>
+        </>
     )
 }
