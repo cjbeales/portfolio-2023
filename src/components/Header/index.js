6c6d9d7536f7e9192e07518f14c75a3735e1005c
Connor Beales <connorj.beales@gmail.com>
2023-11-28 14:55:11 +0000

    Nav button issue fixed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import styles from './header.module.scss';
 import Link from "next/link";
-import { Container, Button } from "@/components";
-import { useIsMobile } from "@/hooks";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Container } from "@/components";
+import { useIsMobile, useIsDesktop } from "@/hooks";
 import { headerMenuItems } from "./MenuItems";
 
 export const Header = () => {
     const [scrolled, setScrolled] = useState(false);
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const isDesktop = useIsDesktop();
     const isMobile = useIsMobile();
     const dynamicScrollValue = isMobile ? 24 : 50;
 
@@ -27,23 +26,15 @@ export const Header = () => {
                     </Link>
                 </div>
                 <nav>
-                    <ul className={`${styles.nav} ${isMenuOpen ? styles.navActive : ''}`}>
-                        {headerMenuItems.map((item, index) => (
+                    <ul className={styles.nav}>
+                        {headerMenuItems.map(({ title, href, isButton }) => (
                             <li
-                                key={index}
-                                className={`${styles.link} ${isMobile ? 'fs--14' : 'fs--18'} ${scrolled ? 'col--secondary' : ''}`
-                                }>
-                                <Link href={item.href}>{item.title}</Link>
+                                key={title}
+                                className={`${isButton ? styles.linkButton : styles.link} ${isDesktop ? 'fs--18' : 'fs--14'}`}
+                            >
+                                <Link href={href}>{title}</Link>
                             </li>
                         ))}
-                        {!isMobile
-                            ?
-                            <Button label={'Get in touch'} href={'#contact'} />
-                            :
-                            <span className={`${styles.link} ${isMobile ? 'fs--14' : 'fs--18'}`}>
-                                <a href='#contact'>Contact</a>
-                            </span>
-                        }
                     </ul>
                 </nav>
             </Container>
