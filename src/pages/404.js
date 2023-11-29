b8d992319a971263077c518006ccaeef70c143e3
Connor Beales <connorj.beales@gmail.com>
2023-11-29 10:34:36 +0000

    404 page added

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import { Layout, UhOh } from '@/components'
+
+export default function FourOhFour() {
+    return (
+        <Layout>
+            <UhOh />
+        </Layout>
+    )
+}
