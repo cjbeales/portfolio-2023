4f79973586b0983ef248c45c1b2f0521b8deec73
Connor Beales <connorj.beales@gmail.com>
2023-11-30 09:51:48 +0000

    Icon size fixed on mobile

--- src/components/TechStack/Tile.js
+++ src/components/TechStack/Tile.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './tech-stack.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useIsDesktop } from '@/hooks';
+import { useIsDesktop, useIsMobile } from '@/hooks';
 import Image from 'next/image';
 
 export const Tile = ({ title, icon, iconWidth = 30, imgSrc, altText }) => {
     const isDesktop = useIsDesktop();
+    const isMobile = useIsMobile();
 
     return (
         <div className={styles.skillset__block}>
@@ -13,7 +14,7 @@ export const Tile = ({ title, icon, iconWidth = 30, imgSrc, altText }) => {
                 {
                     icon ? <FontAwesomeIcon icon={icon} color={'#ffffff'} fixedWidth={true} width={iconWidth} />
                         :
-                        <Image src={imgSrc} alt={altText} width={iconWidth} height={iconWidth} />
+                        <Image src={imgSrc} alt={altText} width={iconWidth} height={isMobile ? 24 : iconWidth} />
                 }
             </div>
             <p className={`bold no--margin white ${isDesktop ? 'fs--14' : 'fs--12'}`}>{title}</p>
